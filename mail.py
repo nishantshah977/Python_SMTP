0b8ea258d1418363e66408ed2bac70405e27477f
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-01-05 17:01:11 +0545

    Update mail.py

--- mail.py
+++ mail.py
@@ -4,5 +4,5 @@ server = smtplib.SMTP('smtp server', 587)
 server.starttls()
 server.login("Your Username", "your password")
 msg = "Your email"
-server.sendmail("Sender Email","Recover email", msg)
+server.sendmail("Sender Email","Receiver email", msg)
 server.quit()
